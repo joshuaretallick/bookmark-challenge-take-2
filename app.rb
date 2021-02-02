8ad923f14d1dd31dd85e4df49924404913123c2e
Joshua Retallick <josh@old-empire.co.uk>
2021-02-02 14:47:59 +0000

    Displays bookmarks at new link /bookmarks

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
+
   get '/' do
     'Bookmark Manager'
+  end
+
+  get '/bookmarks' do
+    bookmarks = [
+            "http://www.makersacademy.com",
+            "http://www.destroyallsoftware.com",
+            "http://www.google.com"
+            ]
+
+    bookmarks.join
   end
 
   run! if app_file == $0
