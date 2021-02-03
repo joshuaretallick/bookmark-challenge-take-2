48570e675535795db85199d21585eb93b1845f20
Joshua Retallick <josh@old-empire.co.uk>
2021-02-03 15:54:42 +0000

    Refactored app.rb to accept params from new .create method. Wrote coresponding test to go with this and updated class

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,8 +1,9 @@
 require 'pg'
 
 class Bookmark
+
   def self.all
-    if ENV['RACK_ENV'] == 'test' 
+    if ENV['RACK_ENV'] == 'test'
       connection = PG.connect(dbname: 'bookmark_challenge_test')
     else
       connection = PG.connect(dbname: 'bookmark_challenge')
@@ -11,4 +12,15 @@ class Bookmark
     result = connection.exec("SELECT * FROM bookmarks;")
     result.map { |bookmark| bookmark['url'] }
   end
+
+  def self.create(url:)
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_challenge_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_challenge')
+    end
+
+    connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+  end
+
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -9,12 +9,20 @@ describe Bookmark do
       connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
       connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
       connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
-      
+
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include("http://www.makersacademy.com")
       expect(bookmarks).to include("http://www.destroyallsoftware.com")
       expect(bookmarks).to include("http://www.google.com")
+    end
+  end
+
+  describe '.create' do
+    it 'create a new bookmark' do
+      Bookmark.create(url: 'http://www.testbookmark.com')
+
+      expect(Bookmark.all).to include 'http://www.testbookmark.com'
     end
   end
 end
