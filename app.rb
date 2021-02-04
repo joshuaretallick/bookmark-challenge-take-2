cb41546720c3afa89dc8f4441110a881e348f70f
Joshua Retallick <josh@old-empire.co.uk>
2021-02-04 15:36:57 +0000

    Added unit test for deleting bookmark, updated class with .delete method, and refactored routes in app.rb

--- app.rb
+++ app.rb
@@ -24,8 +24,7 @@ class BookmarkManager < Sinatra::Base
   end
 
   delete '/bookmarks/:id' do
-    connection = PG.connect(dbname: 'bookmark_challenge_test')
-    connection.exec("DELETE FROM bookmarks WHERE id = #{params['id']}")
+    Bookmark.delete(id: params[:id])
     redirect '/bookmarks'
   end
 
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -34,4 +34,14 @@ class Bookmark
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
+  def self.delete(id:)
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_challenge_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_challenge')
+    end
+    connection.exec("DELETE FROM bookmarks WHERE id = #{id}")
+
+  end
+
 end
